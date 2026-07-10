07b8d2d30ed246f6ff211ce2a4d190d1d2c542c5
Dev Singh <sigmaplus128@gmail.com>
2026-07-10 22:24:11 +0530

    Add files via upload

---
+++ BACKEND/NODE_JS_06(practice_airbnb_dynamic_ui)/routes/hostRouter.js
@@ -0,0 +1,26 @@
+
+// Core Module
+const path = require('path');
+
+// External Module
+const express = require('express');
+const hostRouter = express.Router();
+
+// Local Module
+const rootDir = require("../utils/pathUtil");
+
+hostRouter.get("/add-home", (req, res, next) => {
+  res.render('addHome', {pageTitle: 'Add Home to airbnb'});
+})
+
+const registeredHomes = [];
+
+hostRouter.post("/add-home", (req, res, next) => {
+  console.log('Home Registration successful for:', req.body);
+   const { houseName, price, location, rating, photourl } = req.body;
+   registeredHomes.push({ houseName, price, location, rating, photourl });
+   res.render('homeAdded', {pageTitle: 'Home Added Successfully'});
+})
+
+exports.hostRouter = hostRouter;
+exports.registeredHomes = registeredHomes;
